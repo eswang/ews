f5e131012a6aee6997febce5140e4eda7da39c00
EW <EW>
2018-03-05 23:05:09 -0800

    Use the Blade layouts.app template for the home page and welcome page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,37 +1,12 @@
-<!DOCTYPE html>
-<!-- ng-app specifies the Angular module to use for this page (js/app.js)-->
-<html lang="en" ng-app="merchantApp">
-<head>
-<!-- base tag is necessary when using HTML 5 routing. href tell Angular what
-	directory to start parsing URLs. -->
-<base href="/"/>
-<meta charset="utf-8">
-<title>Products</title>
-<link rel="stylesheet" href="css/bootstrap.min.css"/>
-<link rel="stylesheet" href="css/app.css"/>
-</head>
-<body>
-<div class="container">
-	<div class="navbar" ng-controller="MainMenuController">
-		<div class="navbar-inner">
-			<ul class="nav">
-				<li><a href="#" ng-click="listProducts()">Product List</a></li>
-				<li><a href="#" ng-click="createProduct()">New Product</a></li>
-				<li><a href="#" ng-click="listOrders()">Order List</a></li>
-				<li><a href="#" ng-click="createOrder()">New Order</a></li>
-			</ul>
-		</div>
-	</div>
-	
-	<!-- ng-view is where we will display the templates for this site. -->
-	<ng-view>
-	</ng-view>
+@extends('layouts.app')
 
-</div>
+@section('content')
+    <!-- ng-view is where we will display the templates for this site. -->
+    <ng-view>
+    </ng-view>
+@endsection
 
-<!-- Bootstrap -->
-<script src="lib/bootstrap.min.js"></script>
-
+@section('scripts')
 <!-- AngularJS -->
 <script src="lib/angular/angular.js"></script>
 <!--  AngularJS $resource module -->
@@ -50,6 +25,4 @@
 <script src="js/controllers/NewOrderController.js"></script>
 <script src="js/services/ProductData.js"></script>
 <script src="js/services/OrderData.js"></script>
-
-</body>
-</html>
+@endsection
